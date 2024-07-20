cf4c9e5248ac99c5309f6b0e727c23801285ce41
Filip Vrubel <114007566+FilipVrubel@users.noreply.github.com>
2024-07-20 09:10:55 +0200

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const container = document.querySelector("div");
+
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.classList.toggle("row")
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            square.style.width = `${608 / 16}px`;
+            square.style.height = `${608 / 16}px`;
+            square.classList.toggle("square");
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createGrid();
